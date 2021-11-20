acfddb24420f2adfb7286c142fc9870dacc8e2ed
Valeria-Aguirre-Uzquiano <valeria.aguirre.uzquiano@gmail.com>
2021-11-20 18:13:11 -0400

    subir backend proyecto

---
+++ src/main/java/bo/edu/ucb/ingsoft/FilmRental/FilmRentalApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.FilmRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmRentalApplication.class, args);
+	}
+
+}
